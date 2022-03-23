190468bcc299e4e2329c80b4aba1430d9c3c3e09
islandsexit <hezkydoc@gmail.com>
2022-03-23 16:24:25 +0300

    2 str

--- script.js
+++ script.js
@@ -1,398 +1,156 @@
-$(function () {
-  // setting
-  var debug = false;
 
-  // fake ajax request
-  // $.mockjax({
-  //   url: '/api/tfa',
-  //   dataType: 'json',
-  //   response: function (settings) {
-  //     this.responseText = {
-  //       "ok": (Math.random() >= 0.5) // random true/false
-  //     };
-  //   },
-  //   responseTime: 1000
-  // });
+$(document).ready(function () {
+
+	console.log("ready DOMMM");
+
+	// const progressBox = document.getElementById('progress-box')
+	const btn_submit = document.getElementById('btn_submit')
+	const csrf = document.getElementsByName('csrfmiddlewaretoken')
+	const id = document.getElementById('id')
+	const name = document.getElementById('name')
+	const password = document.getElementById('password')
+	const uploadForm = document.getElementById('uplForm')
+	// const bar = document.getElementsByClassName('processing_bar')
+	// const error_msg = document.getElementsByClassName('error_msg')
+	// const status_bar = document.getElementsByClassName('status_uploading')
+	document.getElementById('spinner').style.display = 'none'
+	// status_bar[0].style.width = 50+'%';
+	// console.log(status_bar[0].style)
+	var btnUpload = $("#upload_file"),
+		btnOuter = $(".button_outer");
+	btnUpload.on("change", function (e) {
+		console.log("if")
+		var ext = btnUpload.val().split('.').pop().toLowerCase();
+		if ($.inArray(ext, ['gif', 'png', 'jpg', 'jpeg']) == -1) {
+			$(".error_msg").text("Не изображение");
+		} else if (document.getElementById('upload_file').files[0].size > 10 * 1024 * 1024) {
+			$(".error_msg").text("Слишком большой файл");
+		}
+		else {
+			$(".error_msg").text("");
+			const loading = document.getElementsByClassName('processing_bar')
+			// btnOuter.addClass("file_uploading");
+			document.getElementById('input_div').style.display = 'none'
+			document.getElementById('spinner').style.display = 'block'
+
+			const uploadedFile = e.target.files[0];
+			
+			const fd = new FormData;
+
+			fd.append('csrfmiddlewaretoken', csrf[0].value)
+			fd.append('upload_file', uploadedFile)
+			fd.append('id', id.value)
+			fd.append('name', name.value)
+			fd.append('password', password.value)
+			var uploadedFileURL = URL.createObjectURL(uploadedFile)
+			
+			$.ajax({
+				type:'POST',
+				url: uploadForm.action,
+				enctype: 'multipart/form-data',
+				data:fd,
+				beforeSend: function(){
+					
+				},
+				xhr: function(){
+					const xhr = new window.XMLHttpRequest();
+					xhr.upload.addEventListener('progress',e=>{
+						// console.log(e)
+						if (e.lengthComputable){
+							
+							var percent = Math.floor(e.loaded / e.total * 100); 
+							if(percent==100){
+								percent = 89
+							}
+							console.log(percent)
+							console.log(loading[0].style.width)
+							if (percent < 90){
+							
+							loading[0].style.width = percent + '%';
+							if (percent>50){
+								status_bar[0].style.width = percent +'%';
+								
+							}
+							
+							}
+
+							
+						}
+					})
+					return xhr
+				},
+				success: function(response){
+					document.getElementById('input_div').style.display = 'block'
+					document.getElementById('spinner').style.display='none'
+					status_bar[0].style.width = 100 + '%';
+					console.log(response)
+					btnOuter.addClass("file_uploaded");
+					// $("#uploaded_view").append('<img src="' + uploadedFileURL + '" />').addClass("show")
+					$("#btn_submit").removeClass("btn-submit")
+					$(".error_msg").text(response['msg'])
+					status_bar[0].style.display = 'none';
+					if (response['result']=='ERROR'){
+						setTimeout(function () {
+							location.reload(false);
+							 },
+							  3000);
+					
+					}
+					else{
+						error_msg[0].style.color='green'
+						$(".error_msg").text(response['msg'])
+						// setTimeout(function () {
+						// 	window.location('/checkin');
+						// 	 }, 2000);
+						
+						
+					}
+					
+				},
+				error: function(error){
+					document.getElementById('spinner').style.display='none'
+					status_bar[0].style.width = 100 + '%';
+					console.log(response)
+					btnOuter.addClass("file_uploaded");
+					// $("#uploaded_view").append('<img src="' + uploadedFileURL + '" />').addClass("show")
+					$("#btn_submit").removeClass("btn-submit")
+					$(".error_msg").text('Ошибка отпраки фото на сервер')
+					setTimeout(function () {
+						location.reload(false);
+						 }, 3000);
+				},
+				cache: false,
+				contentType:false,
+				processData: false,
+
+			})
+
+			// setTimeout(function () {
+			// 	btnOuter.addClass("file_uploaded");
+			// }, 3000);
+			// var uploadedFile = URL.createObjectURL(e.target.files[0]);
+			// setTimeout(function () {
+			// 	$("#uploaded_view").append('<img src="' + uploadedFile + '" />').addClass("show");
+			// }, 3500);
+			// setTimeout(function () {
+			// 	$("#btn_submit").removeClass("btn-submit")
+			// }, 3500);
+		}
+	});
+	$(".file_remove").on("click", function (e) {
+		$("#uploaded_view").removeClass("show");
+		$("#uploaded_view").find("img").remove();
+		btnOuter.removeClass("file_uploading");
+		btnOuter.removeClass("file_uploaded");
+		$("#btn_submit").addClass("btn-submit")
+	});
+
+
+
+	// btn_submit.addEventListener('click', () => {
+		
+	// })
 
 
 
-  // pincode
-  var _pincode = []
-  _req = null;
-
-  // main form
-  var $form = $('#form');
-
-  // pincode group
-  var $group = $form.find('.form__pincode');
-
-  // all input fields
-  var $inputs = $group.find(':input');
-
-  // input fields
-  var $first = $form.find('[name=pincode-1]')
-    , $second = $form.find('[name=pincode-2]')
-    , $third = $form.find('[name=pincode-3]')
-    , $fourth = $form.find('[name=pincode-4]')
-    , $fifth = $form.find('[name=pincode-5]')
-    , $sixth = $form.find('[name=pincode-6]');
-
-  // submit button
-  var $button = $form.find('.button--primary');
-
-  if (document.getElementById('password').value = '0'){
-    $group.addClass('form__group--error');
-    setTimeout(function(){
-      $(this).prop('disabled', false);
-      $group.removeClass('form__group--success form__group--error');
-      $button.attr('disabled', true);
-    }, 3500)
-  }
-
-  // all fields
-  $inputs
-    .on('keyup', function (event) {
-      var code = event.keyCode || event.which;
-
-      if (code === 9 && !event.shiftKey) {
-        // prevent default event
-        event.preventDefault();
-
-        // focus to submit button
-        $('.button--primary').focus();
-      }
-    })
-    .inputmask({
-      mask: '9',
-      placeholder: '',
-      showMaskOnHover: false,
-      showMaskOnFocus: false,
-      clearIncomplete: true,
-      onincomplete: function () {
-        !debug || console.log('inputmask incomplete');
-      },
-      oncleared: function () {
-        var index = $inputs.index(this)
-          , prev = index - 1
-          , next = index + 1;
-
-        if (prev >= 0) {
-          // clear field
-          $inputs.eq(prev).val('');
-
-          // focus field
-          $inputs.eq(prev).focus();
-
-          // remove last nubmer
-          _pincode.splice(-1, 1)
-        } else {
-          return false;
-        }
-
-        !debug || console.log('[oncleared]', prev, index, next);
-      },
-      onKeyValidation: function (key, result) {
-        var index = $inputs.index(this)
-          , prev = index - 1
-          , next = index + 1;
-
-        // focus to next field
-        if (prev < 6) {
-          $inputs.eq(next).focus();
-        }
-
-        !debug || console.log('[onKeyValidation]', index, key, result, _pincode);
-      },
-      onBeforePaste: function (data, opts) {
-        $.each(data.split(''), function (index, value) {
-          // set value
-          $inputs.eq(index).val(value);
-
-          !debug || console.log('[onBeforePaste:each]', index, value);
-        });
-
-        return false;
-      }
-    });
-
-  // first field
-  $('[name=pincode-1]')
-    .on('focus', function (event) {
-      !debug || console.log('[1:focus]', _pincode);
-    })
-    .inputmask({
-      oncomplete: function () {
-        // add first character
-        _pincode.push($(this).val());
-
-        // focus to second field
-        $('[name=pincode-2]').focus();
-
-        !debug || console.log('[1:oncomplete]', _pincode);
-      }
-    });
-
-  // second field
-  $('[name=pincode-2]')
-    .on('focus', function (event) {
-      if (!($first.val().trim() !== '')) {
-        // prevent default
-        event.preventDefault();
-
-        // reset pincode
-        _pincode = [];
-
-        // handle each field
-        $inputs
-          .each(function () {
-            // clear each field
-            $(this).val('');
-          });
-
-        // focus to first field
-        $first.focus();
-      }
-
-      !debug || console.log('[2:focus]', _pincode);
-    })
-    .inputmask({
-      oncomplete: function () {
-        // add second character
-        _pincode.push($(this).val());
-
-        // focus to third field
-        $('[name=pincode-3]').focus();
-
-        !debug || console.log('[2:oncomplete]', _pincode);
-      }
-    });
-
-  // third field
-  $('[name=pincode-3]')
-    .on('focus', function (event) {
-      if (!($first.val().trim() !== '' &&
-        $second.val().trim() !== '')) {
-        // prevent default
-        event.preventDefault();
-
-        // reset pincode
-        _pincode = [];
-
-        // handle each field
-        $inputs
-          .each(function () {
-            // clear each field
-            $(this).val('');
-          });
-
-        // focus to first field
-        $first.focus();
-      }
-
-      !debug || console.log('[3:focus]', _pincode);
-    })
-    .inputmask({
-      oncomplete: function () {
-        // add third character
-        _pincode.push($(this).val());
-
-        // focus to fourth field
-        $('[name=pincode-4]').focus();
-
-        !debug || console.log('[3:oncomplete]', _pincode);
-      }
-    });
-
-  // fourth field
-  $('[name=pincode-4]')
-    .on('focus', function (event) {
-      if (!($first.val().trim() !== '' &&
-        $second.val().trim() !== '' &&
-        $third.val().trim() !== '')) {
-        // prevent default
-        event.preventDefault();
-
-        // reset pincode
-        _pincode = [];
-
-        // handle each field
-        $inputs
-          .each(function () {
-            // clear each field
-            $(this).val('');
-          });
-
-        // focus to first field
-        $first.focus();
-      }
-
-      !debug || console.log('[4:focus]', _pincode);
-    })
-    .inputmask({
-      oncomplete: function () {
-        // add fo fourth character
-        _pincode.push($(this).val());
-
-        // focus to fifth field
-        $('[name=pincode-5]').focus();
-
-        !debug || console.log('[4:oncomplete]', _pincode);
-      }
-    });
-
-  // fifth field
-  $('[name=pincode-5]')
-    .on('focus', function (event) {
-      if (!($first.val().trim() !== '' &&
-        $second.val().trim() !== '' &&
-        $third.val().trim() !== '' &&
-        $fourth.val().trim() !== '')) {
-        // prevent default
-        event.preventDefault();
-
-        // reset pincode
-        _pincode = [];
-
-        // handle each field
-        $inputs
-          .each(function () {
-            // clear each field
-            $(this).val('');
-          });
-
-        // focus to first field
-        $first.focus();
-      }
-
-      !debug || console.log('[5:focus]', _pincode);
-    })
-    .inputmask({
-      oncomplete: function () {
-        // add fifth character
-        _pincode.push($(this).val());
-
-        // focus to sixth field
-        $('[name=pincode-6]').focus();
-
-        !debug || console.log('[5:oncomplete]', _pincode);
-      }
-    });
-
-  // sixth field
-  $('[name=pincode-6]')
-    .on('focus', function (event) {
-      if (!($first.val().trim() !== '' &&
-        $second.val().trim() !== '' &&
-        $third.val().trim() !== '' &&
-        $fourth.val().trim() !== '' &&
-        $fifth.val().trim() !== '')) {
-        // prevent default
-        event.preventDefault();
-
-        // reset pincode
-        _pincode = [];
-
-        // handle each field
-        $inputs
-          .each(function () {
-            // clear each field
-            $(this).val('');
-          });
-
-        // focus to first field
-        $first.focus();
-      }
-
-      !debug || console.log('[6:focus]', _pincode);
-    })
-    .inputmask({
-      oncomplete: function () {
-        // add sixth character
-        _pincode.push($(this).val());
-        
-        
-
-        // pin length not equal to six characters
-        if (_pincode.length !== 6) {
-          // reset pin
-          _pincode = [];
-
-          // handle each field
-          $inputs
-            .each(function () {
-              // clear each field
-              $(this).val('');
-            });
-
-          // focus to first field
-          $('[name=pincode-1]').focus();
-        } else {
-          // handle each field
-          $inputs.each(function () {
-            // disable field
-            $(this).prop('disabled', true);
-          });
-          document.getElementById('password').setAttribute('value',_pincode.join(''));
-          document.getElementById('form-horizontal').submit()
-
-          // // send request
-          // _req = $.ajax({
-          //   type: 'POST',
-          //   url: '/api/tfa',
-          //   data: {
-          //     'code': _pincode.join(''),
-          //     '_csrf': ''
-          //   }
-          // })
-        //     .done(function (data, textStatus, jqXHR) {
-        //       try {
-        //         !debug || console.log('data', data);
-
-        //         if (data.ok === true) {
-        //           $group.addClass('form__group--success');
-        //           $button.removeAttr('disabled');
-        //         }
-
-        //         if (data.ok === false) {
-        //           $group.addClass('form__group--error');
-        //         }
-        //       } catch (err) {
-
-        //       }
-        //     })
-        //     .fail(function (jqXHR, textStatus, errorThrown) {
-        //       $group.removeClass('form__group--error');
-        //     })
-        //     .always(function (dataOrjqXHR, textStatus, jqXHRorErrorThrown) {
-        //       // reset pin
-        //       _pincode = [];
-
-        //       // reset request
-        //       _req = null;
-
-        //       setTimeout(function () {
-        //         // handle each field
-        //         $inputs.each(function () {
-        //           // clear all fields
-        //           $(this).val('');
-
-        //           // enable all fields
-        //           $(this).prop('disabled', false);
-        //         });
-
-        //         // remove response status class
-        //         $group.removeClass('form__group--success form__group--error');
-
-        //         // disable submit button
-        //         $button.attr('disabled', true);
-
-        //         // focus to first field
-        //         $first.focus();
-        //       }, 2000);
-        //     });
-        // }
-
-        !debug || console.log('[6:oncomplete]', _pincode);
-      }
-    }});
 });
